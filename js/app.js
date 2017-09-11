35c8d1846a6eecfa3c657904733e697d739a9a2b
Administrador <imac2@root-Michelle.local>
2017-09-11 05:33:17 -0500

    all

--- js/app.js
+++ js/app.js
@@ -1,2 +1,44 @@
 // var app = angular.module('score_federal', ['ngMaterial', 'chart.js'])
 var app = angular.module('score_federal', ['ngMaterial'])
+
+
+app.controller('TabController', ['$scope', function($scope) {
+    $scope.tab = 1;
+
+    $scope.setTab = function(newTab){
+      $scope.tab = newTab;
+    };
+
+    $scope.isSet = function(tabNum){
+      return $scope.tab === tabNum;
+    };
+}])
+
+
+app.controller('AppCtrl',
+  function($scope) {
+	$scope.accordianData = [
+		{
+			"heading" : "Trabajo Legislativo",
+			"content" : ""
+		},
+		{
+			"heading" : "Rol Politico",
+			"content" : ""
+		},
+		{
+			"heading" : "Extra Legislativo",
+			"content" : ""
+		}
+	];
+
+	$scope.collapseAll = function(data) {
+	   for(var i in $scope.accordianData) {
+		   if($scope.accordianData[i] != data) {
+			   $scope.accordianData[i].expanded = false;
+		   }
+	   }
+	   data.expanded = !data.expanded;
+	};
+
+})
