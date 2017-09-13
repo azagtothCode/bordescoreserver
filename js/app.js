d9ccf0716c1fcceac2e339c6c625dfeb4472b020
Administrador <imac2@root-Michelle.local>
2017-09-13 05:28:10 -0500

    dg

--- js/app.js
+++ js/app.js
@@ -1,17 +1,64 @@
 // var app = angular.module('score_federal', ['ngMaterial', 'chart.js'])
-var app = angular.module('score_federal', ['ngMaterial'])
+var app = angular.module('score_federal', ['ngMaterial', 'chart.js'])
 
 
 app.controller('TabController', ['$scope', function($scope) {
     $scope.tab = 1;
+    if($scope.tab==1){
+      document.getElementById("imageScoreTrabajo").src = "../imgs/svg/scoreblack.svg";
+      document.getElementById("imageGlyphiTrabajo").src = "../imgs/svg/work.svg";
+    }
+
 
     $scope.setTab = function(newTab){
       $scope.tab = newTab;
+
+      if(newTab == 1){
+        document.getElementById("imageScoreTrabajo").src = "../imgs/svg/scoreblack.svg";
+        document.getElementById("imageGlyphiTrabajo").src = "../imgs/svg/work.svg";
+
+        document.getElementById("imageScoreRol").src = "../imgs/svg/scorewhite.svg";
+        document.getElementById("imageGlyphiRol").src = "../imgs/svg/workwhite.svg";
+
+        document.getElementById("imageScoreExtra").src = "../imgs/svg/scorewhite.svg";
+        document.getElementById("imageGlyphiExtra").src = "../imgs/svg/extrawhite.svg";
+      }
+
+      if(newTab == 2){
+        document.getElementById("imageScoreRol").src = "../imgs/svg/scoreblack.svg";
+        document.getElementById("imageGlyphiRol").src = "../imgs/svg/work.svg";
+
+        document.getElementById("imageScoreTrabajo").src = "../imgs/svg/scorewhite.svg";
+        document.getElementById("imageGlyphiTrabajo").src = "../imgs/svg/workwhite.svg";
+
+        document.getElementById("imageScoreExtra").src = "../imgs/svg/scorewhite.svg";
+        document.getElementById("imageGlyphiExtra").src = "../imgs/svg/extrawhite.svg";
+      }
+
+      if(newTab == 3){
+        document.getElementById("imageScoreRol").src = "../imgs/svg/scorewhite.svg";
+        document.getElementById("imageGlyphiRol").src = "../imgs/svg/workwhite.svg";
+
+        document.getElementById("imageScoreTrabajo").src = "../imgs/svg/scorewhite.svg";
+        document.getElementById("imageGlyphiTrabajo").src = "../imgs/svg/workwhite.svg";
+
+        document.getElementById("imageScoreExtra").src = "../imgs/svg/scoreblack.svg";
+        document.getElementById("imageGlyphiExtra").src = "../imgs/svg/extra.svg";
+      }
+
     };
 
     $scope.isSet = function(tabNum){
       return $scope.tab === tabNum;
     };
+
+    $scope.selectedIndex = 0;
+
+  $scope.itemClicked = function ($index) {
+    console.log($index);
+    $scope.selectedIndex = $index;
+  }
+
 }])
 
 
@@ -41,4 +88,31 @@ app.controller('AppCtrl',
 	   data.expanded = !data.expanded;
 	};
 
+})
+
+app.config(['ChartJsProvider', function (ChartJsProvider) {
+    // Configure all charts
+    ChartJsProvider.setOptions({
+      chartColors: ['#990000', '#990000'],
+      responsive: true
+    });
+    // Configure all line charts
+  }])
+
+app.controller("BarCtrlIniciativas", function ($scope) {
+  $scope.labels = ['Aprobadas', 'Presentadas', 'Retiradas', 'Desechadas', 'Pendientes'];
+  $scope.series = ['Iniciativas'];
+
+  $scope.data = [
+    [65, 59, 80, 81, 56]
+  ];
+})
+
+app.controller("BarCtrlPuntos", function ($scope) {
+  $scope.labels = ['Aprobadas', 'Presentadas', 'Retiradas', 'Desechadas', 'Pendientes'];
+  $scope.series = ['P. de Acuerdo'];
+
+  $scope.data = [
+    [65, 59, 80, 81, 56]
+  ];
 })
